db9c99cef2a170d6923b39a8b44ad66634a6b831
Olli Suoniemi <olli.suoniemi@aalto.fi>
2025-09-25 00:33:09 +0300

    Add cache utility with Redis integration for method call caching

---
+++ api/src/util/cacheUtil.js
@@ -0,0 +1,42 @@
+import { connect } from "../deps.js";
+
+let redis = "";
+
+try {
+  redis = await connect({
+    hostname: "redis",
+    port: 6379,
+  });
+  console.log("Connected to Redis");
+} catch (error) {
+  console.error("Error connecting to Redis:", error);
+}
+
+
+const cacheMethodCalls = (object, methodsToFlushCacheWith = []) => {
+  const handler = {
+    get: (module, methodName) => {
+      const method = module[methodName];
+      return async (...methodArgs) => {
+        if (methodsToFlushCacheWith.includes(methodName)) {
+          await redis.flushdb()
+          return await method.apply(this, methodArgs);
+        }
+
+        const cacheKey = `${methodName}-${JSON.stringify(methodArgs)}`;
+        const cacheResult = await redis.get(cacheKey);
+        if (!cacheResult) {
+          const result = await method.apply(this, methodArgs);
+          await redis.set(cacheKey, JSON.stringify(result));
+          return result;
+        }
+
+        return JSON.parse(cacheResult);
+      };
+    },
+  };
+
+  return new Proxy(object, handler);
+};
+
+export { cacheMethodCalls, redis };
