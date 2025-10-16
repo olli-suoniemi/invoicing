e15b2ccb1f3e20d89d3b2209f74da2e74aa30c67
Olli Suoniemi <olli.suoniemi@aalto.fi>
2025-10-16 22:59:20 +0300

    Add RootLayout component for client-side rendering with conditional NavBar

---
+++ ui/app/layout.js
@@ -0,0 +1,30 @@
+'use client';  // Mark the layout as a client-side component
+
+// ui/app/layout.js
+import NavBar from './components/navBar';
+import { usePathname } from 'next/navigation';
+import './globals.css'; // Import global styles
+
+export default function RootLayout({ children }) {
+  const pathname = usePathname(); // Get the current pathname
+
+  const metadata = {
+    icons: {
+      icon: "/favicon.ico",  
+    },
+  };
+
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href={metadata.icons.icon} />
+      </head>
+      <body>
+        {/* Conditionally render NavBar */}
+        {/* If the current route is not login or register, show the NavBar */}
+        {pathname !== '/login' && pathname !== '/register' && <NavBar />}
+        {children} {/* Render the page content */}
+      </body>
+    </html>
+  );
+}
